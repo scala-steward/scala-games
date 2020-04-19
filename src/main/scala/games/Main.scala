3b800fa88949a3888bb385303cbd5a73b511589a
dylan <dylanm@qualtrics.com>
2020-04-19 14:57:35 -0700

    adding initial implementation of the hangman game via FSM

---
+++ src/main/scala/games/Main.scala
@@ -0,0 +1,53 @@
+package games
+
+import cats.data.NonEmptyList
+import cats.effect.{ExitCode, IO, IOApp}
+import com.softwaremill.sttp.quick.{sttp, _}
+import games.Hangman.WordService
+import games.typeClasses.Console
+
+import scala.util.Random
+
+object Main extends IOApp {
+
+  case object ApiWordService extends WordService[IO] {
+    override def getWord: IO[NonEmptyList[Char]] =
+      for {
+        wordApiResponse <- IO(
+          sttp
+            .get(uri"https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
+            .send())
+        words <- (wordApiResponse.body match {
+          case Right(value) => IO.pure(value)
+          case Left(error)  => IO.raiseError(new RuntimeException(error))
+        }).map(_.split('\n'))
+        number <- IO(Random.nextInt(words.length - 1))
+        randomWord = NonEmptyList.fromList(words(number).toList)
+        word <- if (randomWord.isDefined) IO(randomWord.get)
+        else
+          IO.raiseError(new RuntimeException("Word API returning weird values"))
+      } yield word
+  }
+
+  implicit val ioInstance: Console[IO] = new Console[IO] {
+    import scala.io.StdIn
+
+    override def readLine: IO[String] = IO(StdIn.readLine)
+    override def printLine(text: String): IO[Unit] = IO(println(text))
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val ticTacToe = TicTacToe.run[IO]
+    val hangman = Hangman.run[IO] _
+
+    for {
+      _ <- ioInstance.printLine("Available options: TicTacToe, HangMan")
+      name <- ioInstance.readLine
+      exit <- name match {
+        case "ticTacToe" => ticTacToe
+        case "hangMan"   => hangman(ApiWordService)
+        case _           => IO(ExitCode.Success)
+      }
+    } yield exit
+  }
+}
--- src/test/scala/games/TicTacToe/game/GameSpec.scala
+++ src/test/scala/games/TicTacToe/game/GameSpec.scala
@@ -10,132 +10,151 @@ import GameSpec._
 
 class GameSpec extends Specification {
   "Encoding/Decoder" >> {
-    encoderDecoderParity[Game](NonEmptyList.fromListUnsafe(
-      List(Game(
-        Row(Some(X), Some(X), Some(X)),
-        Row(None,    None,    None   ),
-        Row(Some(O), Some(O), Some(O)))).zip(List(
-        "X X X\n" ++
-        ". . .\n" ++
-        "O O O"
+    encoderDecoderParity[Game](
+      NonEmptyList.fromListUnsafe(
+        List(
+          Game(Row(Some(X), Some(X), Some(X)),
+               Row(None, None, None),
+               Row(Some(O), Some(O), Some(O)))).zip(
+          List(
+            "X X X\n" ++
+              ". . .\n" ++
+              "O O O"
+          ))
       ))
-    ))
   }
 
   "Making a move" >> {
-    game.move(game.empty, Move(X, A1)) must beSome(buildGameUnsafe(
-      "X . .",
-      ". . .",
-      ". . ."
-    ))
+    game.move(game.empty, Move(X, A1)) must beSome(
+      buildGameUnsafe(
+        "X . .",
+        ". . .",
+        ". . ."
+      ))
 
-    game.move(game.empty, Move(X, B1)) must beSome(buildGameUnsafe(
-      ". X .",
-      ". . .",
-      ". . ."
-    ))
+    game.move(game.empty, Move(X, B1)) must beSome(
+      buildGameUnsafe(
+        ". X .",
+        ". . .",
+        ". . ."
+      ))
 
-    game.move(game.empty, Move(X, C1)) must beSome(buildGameUnsafe(
-      ". . X",
-      ". . .",
-      ". . ."
-    ))
-
-    // ---------------------------------------
-
-    game.move(game.empty, Move(X, A2)) must beSome(buildGameUnsafe(
-      ". . .",
-      "X . .",
-      ". . ."
-    ))
-
-    game.move(game.empty, Move(X, B2)) must beSome(buildGameUnsafe(
-      ". . .",
-      ". X .",
-      ". . ."
-    ))
-
-    game.move(game.empty, Move(X, C2)) must beSome(buildGameUnsafe(
-      ". . .",
-      ". . X",
-      ". . ."
-    ))
+    game.move(game.empty, Move(X, C1)) must beSome(
+      buildGameUnsafe(
+        ". . X",
+        ". . .",
+        ". . ."
+      ))
 
     // ---------------------------------------
 
-    game.move(game.empty, Move(X, A3)) must beSome(buildGameUnsafe(
-      ". . .",
-      ". . .",
-      "X . ."
-    ))
+    game.move(game.empty, Move(X, A2)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        "X . .",
+        ". . ."
+      ))
 
-    game.move(game.empty, Move(X, B3)) must beSome(buildGameUnsafe(
-      ". . .",
-      ". . .",
-      ". X ."
-    ))
+    game.move(game.empty, Move(X, B2)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        ". X .",
+        ". . ."
+      ))
 
-    game.move(game.empty, Move(X, C3)) must beSome(buildGameUnsafe(
-      ". . .",
-      ". . .",
-      ". . X"
-    ))
+    game.move(game.empty, Move(X, C2)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        ". . X",
+        ". . ."
+      ))
+
+    // ---------------------------------------
+
+    game.move(game.empty, Move(X, A3)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        ". . .",
+        "X . ."
+      ))
+
+    game.move(game.empty, Move(X, B3)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        ". . .",
+        ". X ."
+      ))
+
+    game.move(game.empty, Move(X, C3)) must beSome(
+      buildGameUnsafe(
+        ". . .",
+        ". . .",
+        ". . X"
+      ))
   }
 
   "check if anyone win" >> {
     val wins: Seq[(Game, Option[Square])] = Seq(
       (buildGameUnsafe(
-        "O . O",
-        "X X O",
-        "X O X"
-      ), None),
+         "O . O",
+         "X X O",
+         "X O X"
+       ),
+       None),
       (buildGameUnsafe(
-        "O O O",
-        "X X .",
-        "X O X"
-      ), Some(O)),
+         "O O O",
+         "X X .",
+         "X O X"
+       ),
+       Some(O)),
       (buildGameUnsafe(
-        "O . O",
-        "X X X",
-        "O O X"
-      ), Some(X)),
+         "O . O",
+         "X X X",
+         "O O X"
+       ),
+       Some(X)),
       (buildGameUnsafe(
-        "O . O",
-        "X O O",
-        "X X X"
-      ), Some(X)),
+         "O . O",
+         "X O O",
+         "X X X"
+       ),
+       Some(X)),
       (buildGameUnsafe(
-        "X . O",
-        "X X O",
-        "X O O"
-      ), Some(X)),
+         "X . O",
+         "X X O",
+         "X O O"
+       ),
+       Some(X)),
       (buildGameUnsafe(
-        "O X .",
-        "X X O",
-        "O X X"
-      ), Some(X)),
+         "O X .",
+         "X X O",
+         "O X X"
+       ),
+       Some(X)),
       (buildGameUnsafe(
-        "O . O",
-        "X X O",
-        "X X O"
-      ), Some(O)),
+         "O . O",
+         "X X O",
+         "X X O"
+       ),
+       Some(O)),
       (buildGameUnsafe(
-        "O . X",
-        "X X O",
-        "X O O"
-      ), Some(X)),
+         "O . X",
+         "X X O",
+         "X O O"
+       ),
+       Some(X)),
       (buildGameUnsafe(
-        "X . O",
-        "X X O",
-        "O O X"
-      ), Some(X)),
+         "X . O",
+         "X X O",
+         "O O X"
+       ),
+       Some(X)),
     )
 
-    Fragments.foreach(wins){
+    Fragments.foreach(wins) {
       case (currentGame, winner) => game.winner(currentGame) must_=== winner
     }
   }
-
 
   "tell if board is full" >> {
     val currentGame = buildGameUnsafe(
@@ -151,17 +170,22 @@ class GameSpec extends Specification {
 
   "return None if game state after a move is not valid" >> {
     game.move(buildGameUnsafe(
-      ". . .",
-      ". . .",
-      ". . X"
-    ), Move(X, C3)) must beNone
+                ". . .",
+                ". . .",
+                ". . X"
+              ),
+              Move(X, C3)) must beNone
   }
 }
 
 object GameSpec {
-  def buildGameUnsafe(row1: String, row2: String, row3: String): Game = Decoder[Game].decode(Seq(
-    row1,
-    row2,
-    row3
-  ).mkString("\n")).get
+  def buildGameUnsafe(row1: String, row2: String, row3: String): Game =
+    Decoder[Game]
+      .decode(
+        Seq(
+          row1,
+          row2,
+          row3
+        ).mkString("\n"))
+      .get
 }
